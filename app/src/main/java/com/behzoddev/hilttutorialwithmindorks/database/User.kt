0af76aaee2200bb09edd148823ebd2aa75cbf84c
behzod <behzoddev@gmail.com>
2021-12-21 19:04:25 +0900

    Add user model class

---
+++ app/src/main/java/com/behzoddev/hilttutorialwithmindorks/database/User.kt
@@ -0,0 +1,12 @@
+package com.behzoddev.hilttutorialwithmindorks.database
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class User(
+    @Json(name = "id") val id: Int = 0,
+    @Json(name = "name") val name: String = "",
+    @Json(name = "email") val email: String = "",
+    @Json(name = "avatar") val avatar: String = ""
+)
