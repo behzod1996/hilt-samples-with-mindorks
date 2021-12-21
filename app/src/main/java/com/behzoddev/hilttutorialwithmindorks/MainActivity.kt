eab3aaa98cddea15b5b03c78c857f288a43b2d5d
behzod <behzoddev@gmail.com>
2021-12-21 19:03:44 +0900

    Update with NavHost instance

--- app/src/main/java/com/behzoddev/hilttutorialwithmindorks/MainActivity.kt
+++ app/src/main/java/com/behzoddev/hilttutorialwithmindorks/MainActivity.kt
@@ -2,10 +2,19 @@ package com.behzoddev.hilttutorialwithmindorks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
+    }
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment?
+            ?: return
     }
 }
