69cdd68e0d583132482d60bcb2af240d11307364
behzod <behzoddev@gmail.com>
2021-12-21 19:03:08 +0900

    Add view extension

---
+++ app/src/main/java/com/behzoddev/hilttutorialwithmindorks/common/ViewExtension.kt
@@ -0,0 +1,11 @@
+package com.behzoddev.hilttutorialwithmindorks.common
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toastLong(msg: String) {
+    Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+}
+fun Context.toastShort(msg: String) {
+    Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+}
