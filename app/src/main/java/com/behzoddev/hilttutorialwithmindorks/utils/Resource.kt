a37882fd7313c7c8a5656f5a3c46b026a75ed5b7
behzod <behzoddev@gmail.com>
2021-12-27 14:01:13 +0900

    Update state

--- app/src/main/java/com/behzoddev/hilttutorialwithmindorks/utils/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.behzoddev.hilttutorialwithmindorks.utils
-
-sealed class Resource <out T: Any> {
-    data class OnSuccess<out T: Any>(val data : T) : Resource<T>()
-    data class OnLoading(val isLoading: Boolean) : Resource<Nothing>()
-    data class OnError(val throwable: Throwable) : Resource<Nothing>()
-}
